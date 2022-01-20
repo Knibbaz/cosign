e2739db4b233b765d1212c7d9e4162e9730ed1f5
Knibbaz <info@basabbink.nl>
2022-01-20 21:26:01 +0100

    Test

--- pkg/cosign/verify.go
+++ pkg/cosign/verify.go
@@ -255,6 +255,8 @@ func VerifyImageSignatures(ctx context.Context, signedImgRef name.Reference, co 
 		sigs, err = se.Signatures()
 		fmt.Println()
 		fmt.Println(se.Signatures())
+		fmt.Println(se.Signatures())
+		fmt.Println(se.Signatures())
 		fmt.Println("\n\ncosign/verify.go 255 se.Signatures()", "se:", se, "result:", sigs)
 		if err != nil {
 			return nil, false, err
